bd4241db5df7169840e31cbc5a04e648d6d983ac
ItsTHEAvro <jyotirmoy.avro@gmail.com>
2020-06-19 16:22:35 +0600

    Solved exercise 2-2

---
+++ Solutions/Chapter 02/Exercise 2-2/RectagleDrawTool.c
@@ -0,0 +1,33 @@
+/*
+Collected From: http://www.csc.villanova.edu/~tway/coolc/
+*/
+
+#include <stdio.h>
+
+void drawrect(int w, int h)
+{
+    int i, j;
+    for (i = 0; i < w; i++)
+	putchar('x');
+    putchar('\n');
+    for (i = 2; i < h; i++)
+    {
+        putchar('x');
+        for (j = 2; j < w; j++)
+	    putchar(' ');
+	putchar('x');
+	putchar('\n');
+    }
+    for (i = 0; i < w; i++)
+	putchar('x');
+    putchar('\n');
+}
+
+int main()
+{
+    drawrect(10,15);
+    drawrect(3,5);
+    drawrect(50,20);
+    drawrect(10,10);
+    drawrect(2,2);
+}

66554643ab57346c6278173da64e870c663ddbb5
ItsTHEAvro <jyotirmoy.avro@gmail.com>
2020-06-19 15:57:17 +0600

    Solved exercise 2-1

---
+++ Solutions/Chapter 02/Exercise 2-1.c
@@ -0,0 +1,14 @@
+/*
+  Name: Hello World
+  Description: A simple "Hello World" program that shows "Hello World" to the screen
+  Author: Jyotirmoy Avro
+  GitHub Repo: https://github.com/ItsTHEAvro/Practical-C-Programming-Solutions
+*/
+
+#include <stdio.h> //Including header file for standard I/O
+
+int main() {
+    /* Printing "Hello World" */
+    printf("Hello World\n"); // "\n" to move the cursor to the next line
+    return (0);
+}
